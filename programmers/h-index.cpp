f3c8828dc69215b260fc3911a2011d2ef0d470aa
Jeon,Cheol <real-overcomer-jc@gmail.com>
2021-03-16 18:44:54 +0900

    update h-index.cpp

--- programmers/h-index.cpp
+++ programmers/h-index.cpp
@@ -1,26 +1,25 @@
-#include <string>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
 bool decision(const vector<int>& citations, int h) {
-    for (int i = 0; i < citations.size(); ++i) {
-        if (citations[i] >= h && i+1 >= h) return true;
+    return h > 0 and citations[h-1] >= h;
+}
+
+int parametric_search(const vector<int>& citations, int start, int end) {
+    while (start < end) {
+        int h = (start+end+1)/2;
+        if (decision(citations, h)) {
+            start = h;
+        } else {
+            end = h-1;
+        }
     }
-    return false;
+    return start;
 }
 
 int solution(vector<int> citations) {
     sort(citations.begin(), citations.end(), greater<int>());
-    int lo = 0, hi = 10000;
-    while (lo <= hi) {
-        int mid = lo + (hi-lo)/2;
-        if (decision(citations, mid)) {
-            lo = mid + 1;
-        } else {
-            hi = mid - 1;
-        }
-    }
-    return lo - 1;
+    return parametric_search(citations, 0, citations.size());
 }
